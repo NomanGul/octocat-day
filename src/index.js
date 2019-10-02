f4bb0a2aaa0700e24c18784f5f5a5d7f7b7ce606
nomangul <nomangul2001@gmail.com>
2019-10-02 23:30:32 +0500

    add antd css

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import "antd/dist/antd.css";
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
