2170f7e17008eec58436ef10288e5167bf9f56de
nomangul <nomangul2001@gmail.com>
2019-10-02 23:30:19 +0500

    add screen width/height util

---
+++ src/utils/Dimensions.js
@@ -0,0 +1,9 @@
+export const width =
+  window.innerWidth ||
+  document.documentElement.clientWidth ||
+  document.body.clientWidth
+
+export const height =
+  window.innerHeight ||
+  document.documentElement.clientHeight ||
+  document.body.clientHeight
