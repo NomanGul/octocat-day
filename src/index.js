43f0ce21fe19f92710b914db89ebb2350e423b1e
nomangul <nomangul2001@gmail.com>
2019-10-02 23:55:08 +0500

    remove antd css from main index.js file
    add antd css in main index.css file

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import "antd/dist/antd.css";
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
